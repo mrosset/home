cf6c3f3d997ff2523aa7653b66337e93a14a1191
Mike Rosset <mike.rosset@gmail.com>
2019-09-11 09:35:10 -0700

    autotools: actually include a build path.scm

---
+++ scheme/home/path.scm
@@ -0,0 +1,35 @@
+;; path.scm
+;; Copyright (C) 2017-2019 Michael Rosset <mike.rosset@gmail.com>
+
+;; This file is part of Home
+
+;; Home is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Home is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (home path)
+  #:export (paths // fluid~ ~))
+
+(define // file-name-separator-string)
+
+(define fluid~ (make-fluid (getenv "HOME")))
+
+(define-syntax ~
+  (syntax-rules ()
+    ((~)
+     (fluid-ref fluid~))
+    ((~ exp)
+     (string-append (fluid-ref fluid~) // exp))
+    ((~ . args) #t)
+    ))
+
+(define paths '())
